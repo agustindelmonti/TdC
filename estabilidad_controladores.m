db02d39d9ac138ccaa1cc1df7508ede489da3719
Oscar Dorronzoro <oscar.dorronzoro@gmail.com>
2020-08-18 00:57:13 -0300

    correcciones y cambios menores

--- estabilidad_controladores.m
+++ estabilidad_controladores.m
@@ -14,27 +14,30 @@ for i = 1:length(kp)
     FTLC = feedback(FTLA,1);
     step(FTLC);
 end
+legend('Kp=2', 'Kp=10', 'Kp=45');
 
-figure(2) %root locus
-rlocus(Gp)
+figure(2);
+rlocus(FTLA)
 
 
 %% PI
 kp = 6.77;
-ki = [.5,1.38,10];
+ki = [.5,10,1.38];
 
 figure(3); 
 hold on;
 for i = 1:length(ki)
-    Gc = kp + tf([ki(i)],[1 0]);
+    Gc = kp + tf([ki(i)],[1 0]);  % =(Kp*s + Ki)/s, es decir se introduce un polo en s=0 y un cero en s=Ki/Kp
     
     FTLA = Gp*Gc
     FTLC = feedback(FTLA,1);
     step(FTLC);
 end
+legend('Kp=6.77, Ki=0.5', 'Kp=6.77, Ki=10', 'Kp=6.77, Ki=1.38');
 
 figure(4);
 rlocus(FTLA);
+
 % se anadio un polo en 0, aumentando el tipo de sistema en 1
 
 %% PD
@@ -51,16 +54,17 @@ for i = 1:length(kd)
     FTLC = feedback(FTLA,1);
     step(FTLC);
 end
+legend('Kp=6.847, Kd=0', 'Kp=6.847, Kd=1', 'Kp=6.847, Kd=5');
 
 figure(6);
 rlocus(FTLA);
 
 %% PID
 Kp = 6.178;
-Ki = 1;
-Kd = 0;
+Ki = 1.38;
+Kd = 5;
 
-Gc = Kp + tf([Ki],[1 0]) + tf([kd 0],[1]);
+Gc = Kp + tf([Ki],[1 0]) + tf([Kd 0],[1])
 FTLA = Gp*Gc;
 figure(7);
 rlocus(FTLA);
--- tpi_bode_nyquist.m
+++ tpi_bode_nyquist.m
@@ -1,7 +1,7 @@
 %% Diagramas Bode y Nyquist con PI (controlador elegido)
 num=[2580];
 den=[12664 1];
-Gp = tf(num,den,'InputDelay',2.1); %sin retraso la fase nunca llega a 180 -> nunca se inestabiliza?
+Gp = tf(num,den,'InputDelay',2.1); %sin retraso la fase nunca llega a 180 -> nunca se inestabiliza? -> SI
 
 Kp = 1.001%6.77;
 Ki = 0.0000000000001%1.38;
